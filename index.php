0984614e484e8159898df0c261c4899a5126a177
AlexBonifacio <148979761+AlexBonifacio@users.noreply.github.com>
2023-12-11 00:07:59 +0100

    Add style form

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./style/index.css">
 
     <title>Stage de découverte</title>
 </head>
 <body>
     <header>
-
+        <a href="./inscription.php">Inscription</a>
     </header>
     <main>
         <h1 data-value="MYDIGITALSCHOOL">MYDIGITALSCHOOL</h1>

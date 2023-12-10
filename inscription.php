64b8ee05d0f64bd98b416970f21133f2dd1c63da
AlexBonifacio <148979761+AlexBonifacio@users.noreply.github.com>
2023-12-10 21:12:38 +0100

    Create form page

---
+++ inscription.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+    <section class="slider_area inscription-jpo">
+	<div class="single_slider d-flex align-items-end slider_bg_0">
+		<div class="container no-change">
+			<div class="row justify-content-center container-form">
+				<div class="col-lg-7 col-md-6">
+					<div class="slider_text">
+						<a href="/">
+							<span class="icon-angle-left"></span>
+							Retour
+						</a>
+						<h3 class="text-white">Inscription aux évènements</h3>
+												<p>Pour vous inscrire à l'un de nos évènements, veuillez remplir le formulaire ci-contre.</p>
+
+					</div>
+				</div>
+
+				<div class="col-lg-5 col-md-6">
+					<div class="request_form">
+						<form class="webform-submission-form webform-submission-add-form webform-submission-inscription-aux-jpo-sessions-d-a-form webform-submission-inscription-aux-jpo-sessions-d-a-add-form js-webform-submit-once js-webform-details-toggle webform-details-toggle" data-drupal-selector="webform-submission-inscription-aux-jpo-sessions-d-a-add-form" action="/inscription-aux-jpo-et-sessions-dadmissions" method="post" id="webform-submission-inscription-aux-jpo-sessions-d-a-add-form" accept-charset="UTF-8">
+							<div class="form_0">
+								<div class="info text-right">
+									<p class="text-white">Tous les champs sont obligatoires</p>
+									<h3 class="text-white">Lieu et évènement souhaités</h3>
+								</div>
+
+								
+
+
+	<div class="js-form-item js-form-type-select form-type-select js-form-item-ecole-envisagee form-item-ecole-envisagee form-no-label form-group">
+
+					<label for="edit-ecole-envisagee" class="sr-only js-form-required form-required">École envisagée</label>
+								
+<select class="ecole chk-input webform-entity-select form-select required custom-select rounded-0" data-drupal-selector="edit-ecole-envisagee" data-msg-required="Le champ École envisagée est requis." id="edit-ecole-envisagee" name="ecole_envisagee" required="required" aria-required="true"><option value="" selected="selected">Sélectionnez une école</option><option value="457">Angers</option><option value="458">Annecy</option><option value="460">Bordeaux</option><option value="461">Caen</option><option value="462">Grenoble</option><option value="512">Laval</option><option value="463">Lille</option><option value="464">Lyon</option><option value="465">Melun</option><option value="466">Montpellier</option><option value="513">Nancy</option><option value="467">Nantes</option><option value="468">Nice</option><option value="469">Paris</option><option value="470">Rennes</option><option value="471">Saint-Quentin-en-Yvelines</option><option value="472">Toulouse</option><option value="473">Vannes</option></select>
+									</div>
+
+								
+
+
+	<div class="js-form-item js-form-type-select form-type-select js-form-item-evenement-et-date-de-l-evenement form-item-evenement-et-date-de-l-evenement form-no-label form-group">
+
+					<label for="edit-evenement-et-date-de-l-evenement" class="sr-only js-form-required form-required">Événement et date de l’événement</label>
+								
+<select class="chk-input webform-entity-select form-select required custom-select rounded-0" data-drupal-selector="edit-evenement-et-date-de-l-evenement" data-msg-required="Le champ Événement et date de l’événement est requis." id="edit-evenement-et-date-de-l-evenement" name="evenement_et_date_de_l_evenement" required="required" aria-required="true"><option value="" selected="selected">Sélectionnez un événement</option><option value="39165">STUDENT EXPERIENCE #6 - RENNES du 11/12/2023 à 09:00 - 12/12/2023 à 17:00</option><option value="39164">STUDENT EXPERIENCE #7 - RENNES du 12/12/2023 à 09:00 - 12/12/2023 à 17:00</option><option value="38429">Visite ton futur campus - Vannes du 13/12/2023 à 08:30 - 13/12/2023 à 16:30</option><option value="39166">STUDENT EXPERIENCE #8 - RENNES du 13/12/2023 à 09:00 - 13/12/2023 à 17:00</option><option value="38753">SESSION D&#039;INFORMATION &amp; D&#039;ADMISSION - RENNES du 13/12/2023 à 13:30 - 13/12/2023 à 17:00</option><option value="38269">Session d&#039;information et d&#039;admission - Caen du 13/12/2023 à 13:30 - 13/12/2023 à 17:00</option><option value="38677">Student experience - Bachelor Cycle Web et Multimedia - Communication digitale - Laval du 13/12/2023 à 14:00 - 13/12/2023 à 17:00</option><option value="39003">Journée d&#039;immersion - Nantes du 14/12/2023 à 10:00 - 14/12/2023 à 15:00</option><option value="37874">Salon de l&#039;Etudiant Rouen du 15/12/2023 à 09:00 - 15/12/2023 à 17:00</option><option value="39131">Petit-déjeuner de Noël des étudiants - Campus de Caen du 15/12/2023 à 09:00 - 15/12/2023 à 17:00</option><option value="39066">Journée portes ouvertes - Toulouse du 16/12/2023 à 09:00 - 16/12/2023 à 13:00</option><option value="38722">Stage Découverte web et digital - Montpellier du 16/12/2023 à 09:00 - 16/12/2023 à 14:00</option><option value="39126">Journées Portes Ouvertes - Campus de Bordeaux du 16/12/2023 à 09:30 - 16/12/2023 à 12:30</option><option value="39129">Journée Portes Ouvertes - Nice du 16/12/2023 à 10:00 - 16/12/2023 à 12:30</option><option value="38663">Journée Portes Ouvertes - Lyon du 16/12/2023 à 10:00 - 16/12/2023 à 17:00</option><option value="39089">Samedi de l&#039;orientation - Melun du 16/12/2023 à 10:00 - 16/12/2023 à 17:00</option><option value="38965">Journée portes ouvertes Samedi 16/12 - Montpellier du 16/12/2023 à 10:00 - 16/12/2023 à 13:00</option><option value="38640">JOURNÉE DECOUVERTE METIERS - On t&#039;ouvre nos portes - Paris du 16/12/2023 à 10:00 - 16/12/2023 à 16:00</option><option value="38903">Atelier Motion Design - 19/12/2023 du 19/12/2023 à 13:30 - 19/12/2023 à 16:30</option><option value="39172">Réunion d&#039;information - Rennes du 19/12/2023 à 18:00 - 19/12/2023 à 19:00</option><option value="39104">Journée d&#039;Immersion - Framework - Toulouse du 20/12/2023 à 08:30 - 20/12/2023 à 12:00</option><option value="39105">Journée d&#039;Immersion - Framework - Toulouse du 20/12/2023 à 13:00 - 20/12/2023 à 16:30</option><option value="38970">SESSION D&#039;INFORMATION &amp; D&#039;ADMISSION - RENNES du 20/12/2023 à 13:30 - 20/12/2023 à 17:00</option><option value="38883">Portes Ouvertes Nocturnes du 20/12/2023 à 15:00 - 20/12/2023 à 19:00</option><option value="38966">Immersion Partiels 1ère année : Vendredi 22/12 - Montpellier du 22/12/2023 à 10:00 - 22/12/2023 à 16:00</option><option value="38270">Session d&#039;information et d&#039;admission - Caen du 27/12/2023 à 13:30 - 27/12/2023 à 17:00</option><option value="39141">Journée d&#039;Immersion - Gamification- Toulouse du 02/01/2024 à 08:30 - 02/01/2024 à 12:00</option><option value="39142">Journée d&#039;Immersion - Gamification- Toulouse du 02/01/2024 à 13:00 - 02/01/2024 à 16:30</option><option value="38782">Stage de découverte MyDigitalSchool Grenoble du 03/01/2024 à 08:15 - 04/01/2024 à 17:00</option><option value="39061">Stage découverte web et digital - Bordeaux du 03/01/2024 à 10:00 - 04/01/2024 à 17:00</option><option value="39077">SESSION D&#039;INFORMATION &amp; D&#039;ADMISSION - RENNES du 03/01/2024 à 13:30 - 03/01/2024 à 17:00</option><option value="38680"> Student experience - Bachelor Cycle Web et Multimedia - Communication digitale - Laval du 03/01/2024 à 14:00 - 03/01/2024 à 17:00</option><option value="39143">Journée d&#039;Immersion - Conception UX - Toulouse du 04/01/2024 à 08:30 - 04/01/2024 à 12:00</option><option value="39093">Stage découverte du 04 au 05 janvier 2024 - Rennes du 04/01/2024 à 09:00 - 05/01/2024 à 17:00</option><option value="38737">Stage Découverte web et digital - Angers du 04/01/2024 à 09:00 - 04/01/2024 à 17:00</option><option value="39107">Stage découverte web &amp; digital - Toulouse du 04/01/2024 à 09:00 - 05/01/2024 à 16:30</option><option value="38725">Stage découverte web et digital - Annecy du 04/01/2024 à 09:00 - 05/01/2024 à 16:00</option><option value="38856">Stage découverte web et digital - Paris du 04/01/2024 à 09:00 - 04/01/2024 à 17:00</option><option value="39004">Atelier création d&#039;un objet connecté - Nantes du 04/01/2024 à 10:00 - 04/01/2024 à 17:00</option><option value="39144">Journée d&#039;Immersion - Conception UX - Toulouse du 04/01/2024 à 13:00 - 04/01/2024 à 16:30</option><option value="38896">Journée d&#039;Immersion - Cloud Computing &amp; Architecture Serveur - Toulouse du 05/01/2024 à 08:30 - 05/01/2024 à 12:00</option><option value="39145">Journée d&#039;Immersion - Facilitation graphique - Toulouse du 05/01/2024 à 08:30 - 05/01/2024 à 12:00</option><option value="38857">Stage découverte web et digital - Paris du 05/01/2024 à 09:00 - 05/01/2024 à 17:00</option><option value="38738">Stage Découverte web et digital du 05/01/2024 à 09:00 - 05/01/2024 à 17:00</option><option value="39148">Journée d&#039;Immersion - Design Graphique/Identité Visuelle - Toulouse du 05/01/2024 à 13:00 - 05/01/2024 à 16:30</option><option value="39146">Journée d&#039;Immersion - Facilitation graphique - Toulouse du 05/01/2024 à 13:00 - 05/01/2024 à 16:30</option><option value="39149">Journée d&#039;Immersion - Cloud Computing &amp; Architecture Serveur - Toulouse du 05/01/2024 à 13:00 - 05/01/2024 à 16:30</option><option value="39176">Journée portes ouvertes du 06/01/2024 à 09:00 - 06/01/2024 à 13:00</option><option value="39078">SESSION D&#039;INFORMATION &amp; D&#039;ADMISSION - RENNES du 10/01/2024 à 13:30 - 10/01/2024 à 17:00</option><option value="39174">Salon de l&#039;Étudiant - Lyon Eurexpo du 12/01/2024 à 09:00 - 14/01/2024 à 17:00</option><option value="39127">Soirée Portes Ouvertes - Nantes du 12/01/2024 à 17:00 - 12/01/2024 à 20:00</option><option value="39128">Journée Portes Ouvertes - Nantes du 13/01/2024 à 09:30 - 13/01/2024 à 13:00</option><option value="39090">Samedi de l&#039;orientation - Melun du 13/01/2024 à 10:00 - 13/01/2024 à 17:00</option><option value="39138">Journée Portes Ouvertes - Rennes du 20/01/2024 à 09:00 - 20/01/2024 à 17:00</option><option value="38902">Journée d&#039;Immersion -Cloud Computing AWS &amp; Virtualisation - Toulouse du 23/01/2024 à 08:30 - 23/01/2024 à 12:00</option><option value="39147">Journée d&#039;Immersion - Motion Design - Toulouse du 23/01/2024 à 08:30 - 23/01/2024 à 12:00</option><option value="38891">Stage découverte web et digital - Nice du 26/02/2024 à 09:00 - 27/02/2024 à 17:00</option></select>
+									</div>
+
+
+							</div>
+							<div class="form_1">
+								<div class="info">
+									<h3>Mes informations</h3>
+								</div>
+
+								
+
+
+	<div class="js-form-item js-form-type-textfield form-type-textfield js-form-item-prenom form-item-prenom form-no-label form-group">
+
+					<label for="edit-prenom" class="sr-only js-form-required form-required">Prénom</label>
+								
+<input class="chk-input form-text required custom-input rounded-0 form-control" pattern="^([a-zA-ZÀ-ÖØ-öø-ÿ_\-\ \&#039;]+)$" data-webform-pattern-error="Le champ Prénom contient un(des) caractère(s) non valide(s)" data-drupal-selector="edit-prenom" data-msg-maxlength="Prénom field has a maximum length of 255." data-msg-required="Le champ Prénom est requis." data-msg-pattern="Le champ Prénom contient un(des) caractère(s) non valide(s)" type="text" id="edit-prenom" name="prenom" value="" size="60" maxlength="255" placeholder="Prénom" required="required" aria-required="true" />
+
+									</div>
+
+								
+
+
+	<div class="js-form-item js-form-type-textfield form-type-textfield js-form-item-nom form-item-nom form-no-label form-group">
+
+					<label for="edit-nom" class="sr-only js-form-required form-required">Nom</label>
+								
+<input class="chk-input form-text required custom-input rounded-0 form-control" pattern="^([a-zA-ZÀ-ÖØ-öø-ÿ_\-\ \&#039;]+)$" data-webform-pattern-error="Le champ Nom contient un(des) caractère(s) non valide(s)" data-drupal-selector="edit-nom" data-msg-maxlength="Nom field has a maximum length of 255." data-msg-required="Le champ Nom est requis." data-msg-pattern="Le champ Nom contient un(des) caractère(s) non valide(s)" type="text" id="edit-nom" name="nom" value="" size="60" maxlength="255" placeholder="Nom" required="required" aria-required="true" />
+
+									</div>
+
+								
+
+
+	<div class="js-form-item js-form-type-email form-type-email js-form-item-email form-item-email form-no-label form-group">
+
+					<label for="edit-email" class="sr-only js-form-required form-required">Email</label>
+								
+<input class="chk-input form-email required custom-input rounded-0 form-control" pattern="^([a-zA-Z0-9_\-\.\+]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$" data-webform-pattern-error="Le champ Email contient un(des) caractère(s) non valide(s)" data-drupal-selector="edit-email" data-msg-email="Email ne contient pas une adresse email valide." data-msg-maxlength="Email field has a maximum length of 254." data-msg-required="Le champ Email est requis." data-msg-pattern="Le champ Email contient un(des) caractère(s) non valide(s)" type="email" id="edit-email" name="email" value="" size="60" maxlength="254" placeholder="Email" required="required" aria-required="true" />
+
+									</div>
+
+								
+
+
+	<div class="js-form-item js-form-type-tel form-type-tel js-form-item-telephone form-item-telephone form-no-label form-group">
+
+					<label for="edit-telephone" class="sr-only js-form-required form-required">Téléphone</label>
+								
+<input class="chk-input form-tel required custom-input rounded-0 form-control" pattern="^(0)(?: *\d){9}$" data-webform-pattern-error="Le champ Téléphone contient un(des) caractère(s) non valide(s)" data-drupal-selector="edit-telephone" data-msg-maxlength="Téléphone field has a maximum length of 128." data-msg-required="Le champ Téléphone est requis." data-msg-pattern="Le champ Téléphone contient un(des) caractère(s) non valide(s)" type="tel" id="edit-telephone" name="telephone" value="" size="30" maxlength="128" placeholder="Téléphone" required="required" aria-required="true" />
+
+									</div>
+
+
+																	
+
+
+	<div class="js-form-item js-form-type-select form-type-select js-form-item-niveau form-item-niveau form-no-label form-group">
+
+					<label for="edit-niveau" class="sr-only js-form-required form-required">Niveau d'études</label>
+								
+<select class="chk-input form-select required custom-select rounded-0" data-drupal-selector="edit-niveau" data-msg-required="Le champ Niveau d&#039;études est requis." id="edit-niveau" name="niveau" required="required" aria-required="true"><option value="" selected="selected">Niveau d&#039;études *</option><option value="517">Bac</option><option value="518">Bac + 1</option><option value="519">Bac + 2</option><option value="520">Bac + 3</option><option value="521">Bac + 4</option></select>
+									</div>
+
+								
+								
+								<div class="custom-control custom-checkbox mb-3">
+
+									<label class="custom-control-label" for="edit-accord-rgpd-custom">
+										<input type="checkbox" id="edit-accord-rgpd-custom" name="active_profile" value="1" class="default"/>
+										<div>
+											<strong>Pour valider le formulaire, merci de cocher cette case *</strong>
+										</div>
+									</label>
+
+																	
+
+
+	<div class="js-form-item js-form-type-checkbox checkbox form-check js-form-item-accord-rgpd form-item-accord-rgpd form-no-label">
+				
+		<label  class="js-form-item js-form-type-checkbox checkbox form-check js-form-item-accord-rgpd form-item-accord-rgpd form-no-label form-check-label">
+						
+<input data-webform-required-error="Merci de cocher cette case pour valider votre demande." data-drupal-selector="edit-accord-rgpd" data-msg-required="Merci de cocher cette case pour valider votre demande." type="checkbox" id="edit-accord-rgpd" name="accord_rgpd" value="1" class="form-checkbox required form-control" required="required" aria-required="true" />
+
+			<div class="input-checkmark"></div>
+					</label>
+
+							</div>
+
+
+								</div>
+
+								<div class="d-none">
+									
+<input class="webform-button--submit button button--primary js-form-submit form-submit custom-input rounded-0 form-control" data-drupal-selector="edit-actions-submit" type="submit" id="edit-actions-submit" name="op" value="Envoyer" />
+
+
+
+
+									
+<input autocomplete="off" data-drupal-selector="form-t2-atbte-ujwngjsqo84oargxmfsczvjwlpzf1xygtk" type="hidden" name="form_build_id" value="form-T2_atbTe-UJWNGJSQo84OArgxmFSCZvjwlPZF1XYgtk" class="custom-input rounded-0 form-control" />
+
+									
+									
+<input data-drupal-selector="edit-webform-submission-inscription-aux-jpo-sessions-d-a-add-form" type="hidden" name="form_id" value="webform_submission_inscription_aux_jpo_sessions_d_a_add_form" class="custom-input rounded-0 form-control" />
+
+									
+<input data-drupal-selector="edit-token" type="hidden" name="token" value="" class="custom-input rounded-0 form-control" />
+
+									
+<input data-drupal-selector="edit-action" type="hidden" name="action" value="submit_contact" class="custom-input rounded-0 form-control" />
+
+								</div>
+
+								<button type="submit" class="btn-recaptcha btn btn-primary rounded-0 d-flex align-items-center align-center">
+									<span class="icon-check-w"></span>
+									S'inscrire
+								</button>
+
+								<div class="custom-text">
+									<label class="custom-text-label">
+
+																					En cochant cette case, vous acceptez qu’Eduservices recueille vos données et vous informe des formations et des actualités de l’école.
+										
+																					Pour en savoir plus sur la gestion de vos données personnelles et exercer vos droits, reportez-vous à la notice
+																				<a href="/mentions-legales" title="Mentions Légales">ici</a>.
+
+									</label>
+								</div>
+
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+    </main>
+</body>
+</html>

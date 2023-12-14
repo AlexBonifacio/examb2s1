b5d4ba9e794617667ffe7f728b5f7ddb237b6dd5
AlexBonifacio <148979761+AlexBonifacio@users.noreply.github.com>
2023-12-14 23:18:07 +0100

    Final pull

---
+++ success_form/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="icon" href="https://www.mydigitalschool.com/themes/custom/mds/img/favicon.png?v=1" type="image/png">
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="8;url=https://www.mydigitalschool.com/ecole-multimedia-annecy" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/success_form.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<!-- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"> -->
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap">
+    <title>Validation de formulaire</title>
+</head>
+<body>
+<header class="bandeau flex" id="header">
+	<img src="../img/logo_mds_1.png" alt="Logo MYDIGITALSCHOOL">
+	
+</header>
+    <h1 class="flex main_h1">Merci d'avoir envoyé ce formulaire</h1>
+    <p class="flex main_p">Vous allez recevoir un e-mail de confirmation</p>
+    <p class='flex main_p1' >Vous allez être redirigé dans quelques instants</p>
+	<section class="container1 flex">
+    
+	<div class="blur"></div>
+	<section class="information-decouverte">
+
+    </section>
+
+    
+</body>
+</html>
---
+++ video/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        video {
+            height: 100vh;
+        }
+    </style>
+    <title>Page de vidéo</title>
+</head>
+<body>
+<div style="text-align: center;">
+    <video controls>
+        <source src="../img/video_stage_immersion.mp4" type="video/mp4">
+        Votre navigateur ne supporte pas la balise vidéo.
+    </video>
+</div>
+
+</body>
+</html>
